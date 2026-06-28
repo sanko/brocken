3e23edecc3206d13fbd432fe7882464d33401963
Sanko Robinson <sanko@cpan.org>
2026-06-28 08:40:02 -0400

    dragonflybsd: use libpthread.so.0 directly instead of probing libthread_xu.so.2
    libthread_xu.so.2 is in /usr/lib/thread/ which isn't in the runtime
    linker's default search path. libpthread.so.0 is a symlink in /usr/lib/
    pointing to the real implementation, so it's discoverable by ld.so.
    
    [runner:dragonflybsd] [test:t/1000_katsuro/1050_integration.t]

--- lib/Brocken/Jenny/Linker/ELF64.pm
+++ lib/Brocken/Jenny/Linker/ELF64.pm
@@ -516,26 +516,7 @@ Brocken::Jenny::Linker::ELF64 - 64-bit Executable and Linkable Format Generator
             my $libpthread;
 
             if ( $platform->is_dragonflybsd ) {
-
-                # DragonFlyBSD: libpthread.so is a proxy with weak stub symbols
-                # (from libpthread/dummy.c). Link directly against libthread_xu.so
-                # (the real threading implementation) instead.
-                $libpthread = 'libpthread.so';
-                for my $dir ( '/usr/lib/thread', '/usr/lib' ) {
-                    next unless -d $dir;
-                    if ( opendir my $dh, $dir ) {
-                        my @matches = sort { length($b) <=> length($a) } grep { /^libthread_xu\.so/ } readdir $dh;
-                        closedir $dh;
-                        for my $m (@matches) {
-                            my $soname = _elf_soname("$dir/$m");
-                            if ($soname) {
-                                $libpthread = $soname;
-                                last;
-                            }
-                        }
-                        last if $libpthread ne 'libpthread.so';
-                    }
-                }
+                $libpthread = 'libpthread.so.0';
             }
             else {
                 $libpthread = $platform->is_freebsd || $platform->is_midnightbsd ? 'libthr.so.3' : 'libpthread.so.0';
