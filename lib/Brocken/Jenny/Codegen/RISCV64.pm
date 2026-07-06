cc72735f59dab85fd684453b3cde884ff127bbf1
Sanko Robinson <30663+sanko@users.noreply.github.com>
2026-07-06 18:37:34 -0400

    rebuild movsx to handle imm operands with proper fallback names for unnamed icmp ops  [arch:risc]

--- lib/Brocken/Jenny/Codegen/RISCV64.pm
+++ lib/Brocken/Jenny/Codegen/RISCV64.pm
@@ -557,8 +557,40 @@ class Brocken::Jenny::Codegen::RISCV64 {
                     my $src_bits = $src->type ? $src->type->bits : 64;
                     my $dst_r    = $resolve->($dst);
                     my $did      = $reg_id->($dst_r);
-                    my $src_r    = $resolve->($src);
-                    my $sid      = $reg_id->($src_r);
+                    my $src_r;
+                    my $sid;
+                    if ( $src->kind eq 'imm' ) {
+                        my %used;
+                        @used{ values %$assignment } = ();
+                        my $tmp_r;
+                        for my $r ( $platform->registers('caller')->@* ) { $tmp_r = $r, last unless exists $used{$r} }
+                        die 'no temp register for imm operand' unless $tmp_r;
+                        $sid = $reg_id->($tmp_r);
+                        my $val = $src->value;
+                        if ( $val >= -2048 && $val <= 2047 ) {
+                            my $imm = $val & 0xFFF;
+                            $bytes .= pack( 'V', ( $imm << 20 ) | ( 0 << 15 ) | ( 0 << 12 ) | ( $sid << 7 ) | OP_IMM );
+                        }
+                        else {
+                            my $tmp = $val & 0xFFFFFFFFFFFFFFFF;
+                            my @chunks;
+                            while ( $tmp != 0 ) {
+                                push @chunks, $tmp & 0x7FF;
+                                $tmp >>= 11;
+                            }
+                            my $first = pop @chunks;
+                            $bytes .= pack( 'V', ( ( $first & 0xFFF ) << 20 ) | ( 0 << 15 ) | ( 0 << 12 ) | ( $sid << 7 ) | OP_IMM );
+                            for my $chunk ( reverse @chunks ) {
+                                $bytes .= pack( 'V', ( 11 << 20 ) | ( $sid << 15 ) | ( 1 << 12 ) | ( $sid << 7 ) | OP_IMM );
+                                $bytes .= pack( 'V', ( ( $chunk & 0xFFF ) << 20 ) | ( $sid << 15 ) | ( 0 << 12 ) | ( $sid << 7 ) | OP_IMM );
+                            }
+                        }
+                        $src_r = $tmp_r;
+                    }
+                    else {
+                        $src_r = $resolve->($src);
+                        $sid   = $reg_id->($src_r);
+                    }
                     if ( $src_bits <= 8 ) {
 
                         # slli rd, rs, 56; srai rd, rd, 56
