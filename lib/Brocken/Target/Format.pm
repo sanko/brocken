f3a281f75cf9c026a6386dd705e4366e0b89c46f
Sanko Robinson <30663+sanko@users.noreply.github.com>
2026-07-06 08:45:45 -0400

    Cleanup.

--- lib/Brocken/Target/Format.pm
+++
@@ -1,8 +0,0 @@
-use v5.40;
-use feature 'class';
-no warnings 'portable', 'experimental::class';
-#
-class Brocken::Target::Format {
-    method write_bin( $filename, $text, $data, $arch, $os ) { }
-};
-1
